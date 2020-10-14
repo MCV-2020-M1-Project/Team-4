6f49bfe58456a913814cdf7ba117d8e87f981532
Kevin Martín <kevinmf94@gmail.com>
2020-10-14 20:01:32 +0200

    BackgroundRemoval commented

--- week1/background_removal.py
+++ week1/background_removal.py
@@ -3,9 +3,11 @@ import numpy as np
 import imutils
 
 
+# Class to remove backgrounds and evaluate masks
 class BackgroundRemove(object):
     __count = 1
 
+    # Method 1 using Edge detector
     @staticmethod
     def method1(image, show_output=False):
 
@@ -37,6 +39,7 @@ class BackgroundRemove(object):
 
         return mask
 
+    # Method 2 using Threshold and Morphological detector
     @staticmethod
     def method2(image, morph_type=2, show_output=False):
 
@@ -51,7 +54,7 @@ class BackgroundRemove(object):
         gaussian = cv2.GaussianBlur(resize, (3, 3), 0)
         sharpness = cv2.filter2D(gaussian, cv2.CV_8U, np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]))
         threshold = cv2.threshold(sharpness, 90, 255, cv2.THRESH_BINARY_INV)[1]
-        morphological = BackgroundRemove.apply_morph(threshold, 3)
+        morphological = BackgroundRemove.apply_morph(threshold, morph_type)
 
         # Detect contours
         (contours, hierarchy) = cv2.findContours(morphological, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -79,17 +82,18 @@ class BackgroundRemove(object):
 
         return mask
 
+    # Apply to a image different morphological transformations and returns this one
     @staticmethod
     def apply_morph(image, morph_type):
 
-        # Test1
+        # Method1: Two closes of different size
         if morph_type == 1:
             image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)),
                                      iterations=1)
             image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5)),
                                      iterations=3)
 
-        # Test2
+        # Method2: One close in x axis
         if morph_type == 2:
             image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, np.array([
                 [0, 0, 0, 0, 0],
@@ -99,14 +103,11 @@ class BackgroundRemove(object):
                 [0, 0, 0, 0, 0]
             ], dtype=np.uint8), iterations=3)
 
+        # Method3: One close with CROSS structure and 15 iterations
         if morph_type == 3:
-            image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, np.array([
-                [0, 0, 1, 0, 0],
-                [0, 0, 1, 0, 0],
-                [1, 1, 1, 1, 1],
-                [0, 0, 1, 0, 0],
-                [0, 0, 1, 0, 0]
-            ], dtype=np.uint8), iterations=15)
+            image = cv2.morphologyEx(image, cv2.MORPH_CLOSE,
+                                     cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5), dtype=np.uint8),
+                                     iterations=15)
 
         return image
 
@@ -115,6 +116,7 @@ class BackgroundRemove(object):
 
         mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
 
+        # Detect the biggest contour and draw this in the mask image
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
             c = c.astype("float")
@@ -130,10 +132,14 @@ class BackgroundRemove(object):
         cropped_img = image[rect[1]:(rect[1] + rect[3]), rect[0]:(rect[0] + rect[2])]
         return cropped_img
 
+    # Main method to remove a background, returns the image cropped and the mask
     @staticmethod
     def remove_background(image, show_output=False):
 
+        # Apply the best method (Method 2)
         mask = BackgroundRemove.method2(image, 2, show_output)
+
+        # Generate image cropped
         image_crop = BackgroundRemove.crop_with_mask(image, mask)
 
         if show_output:
@@ -143,12 +149,16 @@ class BackgroundRemove(object):
 
         return image_crop, mask
 
+    # Method to evaluate the mask, returns precision, recall and f1-score indexes
     @staticmethod
     def evaluate_mask(original_mask, generated_mask):
+
+        # Calculate True-Positive, False-Positive and False-Negative
         tp = np.sum(np.logical_and(original_mask, generated_mask))
         fp = np.sum(np.logical_and(np.logical_not(original_mask), generated_mask))
         fn = np.sum(np.logical_and(original_mask, np.logical_not(generated_mask)))
 
+        # Calculate precision, recall and f1-score
         precision = tp / (tp + fp)
         recall = tp / (tp + fn)
         f1_score = 2 * ((precision * recall) / (precision + recall))
