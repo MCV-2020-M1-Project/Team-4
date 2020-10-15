7d743302e689233bcdda3022b72a62051e19be2e
rachidboukir <rachidboukir@gmail.com>
2020-10-15 21:27:04 +0200

    Update histogram_distance.py

--- week1/query_images/histogram_distance.py
+++ week1/query_images/histogram_distance.py
@@ -1,16 +1,12 @@
 import math
+import numpy as np
 
 
 class HistogramDistance(object):
 
     @staticmethod
-    def euclidean(h1, h2):  # Euclidean distance
-        result = 0
-        for k in range(len(h1)):
-            dif = (h1[k] - h2[k]) ** 2
-            result += dif
-
-        return math.sqrt(result)
+    def euclidean2(h1, h2):  # Euclidean distance
+        return np.linalg.norm(h1 - h2)
 
     @staticmethod
     def x2distance(h1, h2):  # x^2 distance
